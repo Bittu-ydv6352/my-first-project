e9f5d58c037203b08a07800d65793e78f98ac8e6
Deepanshu <dy8634557@gmail.com>
2026-06-07 07:29:59 +0530

    01: variables code

---
+++ My first project/01_basics/01_variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name = "Bittu";
+    int age = 18;
+
+    cout<<"My name is:"<<name<<endl;
+    cout<<"age is:"<<18<<endl;
+    return 0;
+}
