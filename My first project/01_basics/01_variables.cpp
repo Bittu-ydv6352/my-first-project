ebdc8ded920240147886c4b93c3f5b6ce94fa2e0
Deepanshu <dy8634557@gmail.com>
2026-06-07 19:57:15 +0530

    Basics: Completed variables with all data types

--- My first project/01_basics/01_variables.cpp
+++ My first project/01_basics/01_variables.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
 
     string name = "Bittu";
     int age = 18;
+    float height = 5.9;
+    double weight = 55.75;
+    char grade = 'A';
+    bool isStudent = true;
 
     cout<<"My name is:"<<name<<endl;
     cout<<"age is:"<<age<<endl;
+    cout<<"height is"<<height<<endl;
+    cout<<"weight is "<<weight<<endl;
+    cout<<"grade is "<<grade<<endl;
+    cout<<"student? "<<isStudent<<endl;
     return 0;
 }
